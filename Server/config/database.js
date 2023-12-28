d2bd2a8fea8c6cbe4dd287e6f014255e85fe7b53
Inam10 <inamm94@gmail.com>
2023-12-29 02:03:28 +0530

    Fix some bugs

--- Server/config/database.js
+++ Server/config/database.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-exports.connect = () =>{
-    mongoose.connect(process.env.MONGODB_URL ,{
+exports.connect = () => {
+    mongoose.connect(process.env.MONGODB_URL, {
         useNewUrlParser: true,
         useUnifiedTopology:true,
     })
-    .then(()=>console.log("DB Connect Successfully"))
-    .catch((error)=>{
-        console.log("Db Connection Failed");
+    .then(() => console.log("DB Connected Successfully"))
+    .catch( (error) => {
+        console.log("DB Connection Failed");
         console.error(error);
         process.exit(1);
-    })
-}
+    } )
+};
--- Server/middlewares/auth.js
+++ Server/middlewares/auth.js
@@ -1,99 +1,102 @@
-const  jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const User = require("../models/User");
 
 //auth
-exports.auth = async (req ,res ,next) =>{
-    try {
+exports.auth = async (req, res, next) => {
+    try{
         //extract token
-        const token  =  req.cookies.token || req.body.token || req.header("Authorisation").replace("Bearer" , "");
-        //if token is missing , then response
-        if(!token){
+        const token = req.cookies.token 
+                        || req.body.token 
+                        || req.header("Authorisation").replace("Bearer ", "");
+
+        //if token missing, then return response
+        if(!token) {
             return res.status(401).json({
                 success:false,
-                message: "Token is missing.",
-            })
+                message:'TOken is missing',
+            });
         }
 
         //verify the token
-        try {
+        try{
             const decode =  jwt.verify(token, process.env.JWT_SECRET);
             console.log(decode);
             req.user = decode;
-        } catch (error) {
-            //verification issue
+        }
+        catch(err) {
+            //verification - issue
             return res.status(401).json({
                 success:false,
-                message:"Token is invalid",
-            })
+                message:'token is invalid',
+            });
         }
         next();
-    } 
-    catch (error) {
+    }
+    catch(error) {  
         return res.status(401).json({
             success:false,
-            message: "Something went wrong , while validating the token"
+            message:'Something went wrong while validating the token',
         });
     }
 }
 
 //isStudent
-
-exports.isStudent = async(req, res ,next)=>{
-    try {
-        if(req.user.accountType !== "Student"){
+exports.isStudent = async (req, res, next) => {
+ try{
+        if(req.user.accountType !== "Student") {
             return res.status(401).json({
-                success: false,
-                message: "This is protect route for Student only",
-            })
+                success:false,
+                message:'This is a protected route for Students only',
+            });
         }
         next();
-    }
-     catch (error) {
-        return res.status(500).json({
-            success:false,
-            message:"User role connot be verified."
-        })
-    }
-} 
+ }
+ catch(error) {
+    return res.status(500).json({
+        success:false,
+        message:'User role cannot be verified, please try again'
+    })
+ }
+}
+
 
 //isInstructor
-
-
-exports.isInstructor = async(req, res ,next)=>{
-    try {
-        if(req.user.accountType !== "Instructor"){
-            return res.status(401).json({
-                success: false,
-                message: "This is protect route for Instructor only",
-            })
-        }
-        next();
+exports.isInstructor = async (req, res, next) => {
+    try{
+           if(req.user.accountType !== "Instructor") {
+               return res.status(401).json({
+                   success:false,
+                   message:'This is a protected route for Instructor only',
+               });
+           }
+           next();
     }
-     catch (error) {
-        return res.status(500).json({
-            success:false,
-            message:"User role connot be verified."
-        })
+    catch(error) {
+       return res.status(500).json({
+           success:false,
+           message:'User role cannot be verified, please try again'
+       })
     }
-} 
+   }
+
 
 //isAdmin
-
-exports.isAdmin = async(req, res ,next)=>{
-    try {
-        if(req.user.accountType !== "Admin"){
-            return res.status(401).json({
-                success: false,
-                message: "This is protect route for Admin only",
-            })
-        }
-        next();
+exports.isAdmin = async (req, res, next) => {
+    try{    
+           console.log("Printing AccountType ", req.user.accountType);
+           if(req.user.accountType !== "Admin") {
+               return res.status(401).json({
+                   success:false,
+                   message:'This is a protected route for Admin only',
+               });
+           }
+           next();
     }
-     catch (error) {
-        return res.status(500).json({
-            success:false,
-            message:"User role connot be verified."
-        })
+    catch(error) {
+       return res.status(500).json({
+           success:false,
+           message:'User role cannot be verified, please try again'
+       })
     }
-} 
+   }
