54eac3e6bdea6d35215b89b6cbc850a5aaf894b1
Inam10 <inamm94@gmail.com>
2024-01-01 14:33:16 +0530

    frontend development

--- StudyNotion/src/components/core/HomePage/CodeBlocks.jsx
+++ StudyNotion/src/components/core/HomePage/CodeBlocks.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import CTAButton from "./Button";
 import HighlightText from "./HighlightText";
 import { FaArrowRight } from "react-icons/fa";
+import { TypeAnimation } from "react-type-animation";
 
 const CodeBlocks = ({
   positon,
@@ -9,7 +10,7 @@ const CodeBlocks = ({
   subheading,
   ctabtn1,
   ctabtn2,
-  codebloack,
+  codeblock,
   backgroundGradinat,
   codeColor,
 }) => {
@@ -30,6 +31,40 @@ const CodeBlocks = ({
           <CTAButton active={ctabtn2.active} linkto={ctabtn2.linkto}>
             {ctabtn2.btnText}
           </CTAButton>
+        </div>
+      </div>
+
+      {/* Section 2 */}
+      <div className="h-fit flex flex-row text-10[px] w-[100%] py-4 lg:w-[500px]">
+        {/* create Gradiant */}
+        <div className="text-center flex flex-col w-[10%] text-richblack-400 font-inter font-bold ">
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+          <p>4</p>
+          <p>5</p>
+          <p>6</p>
+          <p>7</p>
+          <p>8</p>
+          <p>9</p>
+          <p>10</p>
+          <p>11</p>
+        </div>
+        <div
+          className={`w-[90%] flex flex-col gap-2 font-bold font-mono ${codeColor} pr-2`}
+        >
+          <TypeAnimation
+            sequence={[codeblock, 2000, ""]}
+            repeat={Infinity}
+            cursor={true}
+            style={
+            {
+            whiteSpace:"pre-line",
+            display: "block"
+            }
+            }
+            omitDeletionAnimation={true}
+          />
         </div>
       </div>
     </div>
