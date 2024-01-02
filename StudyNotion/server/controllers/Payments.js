32140dc192195a6ad88dab065027976bc30fb862
Inam10 <inamm94@gmail.com>
2024-01-02 12:29:53 +0530

    code changges

--- StudyNotion/server/controllers/Payments.js
+++ StudyNotion/server/controllers/Payments.js
@@ -135,8 +135,8 @@ exports.verifySignature = async (req, res) => {
                 //mail send krdo confirmation wala 
                 const emailResponse = await mailSender(
                                         enrolledStudent.email,
-                                        "Congratulations from InamsCode",
-                                        "Congratulations, you are onboarded into new InamsCode Course",
+                                        "Congratulations from CodeHelp",
+                                        "Congratulations, you are onboarded into new CodeHelp Course",
                 );
 
                 console.log(emailResponse);
--- StudyNotion/src/components/core/HomePage/HighlightText.jsx
+++ StudyNotion/src/components/core/HomePage/HighlightText.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React from 'react'
 
-const HighlightText = ({ text }) => {
+const HighlightText = ({text}) => {
   return (
-    <span className="font-bold text-richblue-500">
-      {" "} {text}
+    <span className='font-bold text-richblue-200'>
+        {" "}
+        {text}
     </span>
-  );
-};
+  )
+}
 
-export default HighlightText;
+export default HighlightText
